44cd77c48759328375a089a4edb269044d216a4f
ashishkumar.behera@quadbtech.com <ashishkumar.behera@quadbtech.com>
2024-05-07 00:03:21 +0530

    Faqs, Hamburger-Menu for mobile Added

--- src/manipur_edu_frontend/src/components/BackgroudPage.jsx
+++ src/manipur_edu_frontend/src/components/BackgroudPage.jsx
@@ -5,8 +5,8 @@ import Footer from "./Footer";
 function Background({ children }) {
   return (
     <div>
+      <Navbar />
       <div className="overflow-hidden">
-        <Navbar />
         <div className="m-4 mb-0 relative rounded-3xl bg-[#E5F1FF] ">
           <div className="absolute right-0 -top-1/4 transform translate-x-1/3 w-[80vw] h-[1000px] rounded-full bg-gradient-to-b from-[#cfe5ff] to-[#DCECFF00]"></div>
           {children}
