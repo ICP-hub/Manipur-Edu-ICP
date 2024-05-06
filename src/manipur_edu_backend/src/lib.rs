d07862515201ae4d0407ebdb629be06e80d68ca9
anshika-Quadbcode <anshika.goel@quadbtech.com>
2024-05-06 18:18:48 +0530

    get_private_key func changed

--- src/manipur_edu_backend/src/lib.rs
+++ src/manipur_edu_backend/src/lib.rs
@@ -1,10 +1,7 @@
 // use candid::{CandidType, Deserialize};
-use candid::{CandidType, Decode, Deserialize, Encode};
-use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
-use ic_stable_structures::storable::Bound;
-use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap, Storable};
-// use stable_structures::{Ic0StableMemory, RestrictedMemory, StableBTreeMap, Storable};
-use std::{borrow::Cow, cell::RefCell};
+use candid::{CandidType, Deserialize};
+
+use std::cell::RefCell;
 use ic_cdk::api;
 use ic_cdk::api::caller;
 use ic_cdk::storage;
@@ -13,7 +10,7 @@ use serde::Serialize;
 use serde_json;
 
 use std::collections::HashMap;
-use std::hash::Hash;
+
 pub mod admin;
 pub mod institute;
 pub mod student;
@@ -29,11 +26,6 @@ use certificate::*;
 pub mod scholarship;
 use scholarship::*;
 
-// type Memory = RestrictedMemory<Ic0StableMemory>;
-type Memory = VirtualMemory<DefaultMemoryImpl>;
-
-const MAX_KEY_SIZE: u32 = 8;
-const MAX_VALUE_SIZE: u32 = 100;
 
 // State of the Canister
 #[derive(Serialize, CandidType, Deserialize, Debug, Clone, Default)]
@@ -56,94 +48,12 @@ pub struct State {
     
 }  
 
-#[derive(Serialize, CandidType, Deserialize, Debug, Clone, Default)]
-pub struct Chunk {
-    chunk_value: Vec<u8>,
-    next_chunkid: u32,
-   
-}
-
-impl Storable for Chunk {
-    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
-        Cow::Owned(Encode!(self).unwrap())
-    }
-
-    fn from_bytes(bytes: Cow<[u8]>) -> Self {
-        Decode!(&bytes, Self).unwrap()
-    }
-
-    const BOUND: Bound = Bound::Bounded {
-        max_size: MAX_VALUE_SIZE,
-        is_fixed_size: false,
-    };
-}
-
-
-// impl Storable for Chunk {
-//     fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
-//         Cow::Owned(Encode!(self).unwrap())
-//     }
-
-//     fn from_bytes(bytes: Vec<u8>) -> Self {
-//         Decode!(&bytes, Self).unwrap()
-//     }
-// }
-
-// impl Storable for Chunk {
-//     // const BOUND: Bound = 1024;
-//     fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
-//         std::borrow::Cow::Owned(Encode!(&self.chunk_value, &self.next_chunkid).unwrap())
-//     }
-
-//     fn from_bytes(bytes: Vec<u8>) -> Self {
-//         let (chunk_value, next_chunkid) = Decode!(&bytes, Vec<u8>, u32).unwrap();
-//         Chunk {
-//             chunk_value,
-//             next_chunkid,
-//         }
-//     }
-// }
-
 
 thread_local! {
     pub static STATE: RefCell<State> = RefCell::new(State::default());
 
-    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
-    RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
-
-    static FILE_STABLE_STATE: RefCell<StableBTreeMap<(u32,u32), Chunk , Memory>> = RefCell::new(
-        StableBTreeMap::init(
-            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1))),
-        )
-
-    );
-    
-    // static MAP: RefCell<StableBTreeMap<(u32,u32), Chunk, Memory>> = RefCell::new(
-    //     StableBTreeMap::init(RestrictedMemory::new(Ic0StableMemory, 0..99), MAX_KEY_SIZE, MAX_VALUE_SIZE)
-    // );
-
 }
 
-
-#[ic_cdk_macros::query]
-fn get_image(key1: u32, key2: u32) -> Option<Chunk> {
-    FILE_STABLE_STATE.with(|p| {
-        let map_ref = p.borrow();
-        let value = map_ref.get(&(key1, key2));
-        value
-    })
-}
-// fn get_image(key1: u32, key2: u32) -> Option<Vec<u8>,u32> {
-//     FILE_STABLE_STATE.with(|p| p.borrow().get(&(key1, key2)))
-// }
-
-// Inserts an entry into the map and returns the previous value of the key if it exists.
-#[ic_cdk_macros::update]
-fn upload_image(key1: u32, key2: u32, chunk: Chunk) -> String{
-    
-    FILE_STABLE_STATE.with(|p| p.borrow_mut().insert((key1, key2), chunk));
-    "Successful".to_string()
-}
 
 #[query]
 fn calculate_state_size() -> usize {
@@ -175,17 +85,13 @@ fn add_private_key(private_key: String) -> Result<(), String> {
 }
 
 #[query]
-fn get_private_key(principal_id: String) -> Result<String, String> {
+fn get_private_key() -> Result<String, String> {
     let caller_id = caller().to_string();
-
-    if caller_id != principal_id {
-        return Err("Access denied. Caller is not authorized to access the private key.".to_string());
-    }
 
     STATE.with(|state| {
         let state = state.borrow();
 
-        if let Some(private_key) = state.private_keys.get(&principal_id) {
+        if let Some(private_key) = state.private_keys.get(&caller_id) {
             Ok(private_key.clone())
         } else {
             Err("User not found.".to_string())
